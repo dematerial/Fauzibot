09357779fd4936df7fdfd1359c0d03611545098c
dematerial <36230364+dematerial@users.noreply.github.com>
2018-02-08 20:29:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,11 +12,17 @@ client.on('ready', () => {
 client.on('message', message => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
+  if (message.content.startsWith(config.prefix + "keyword")) {
+    message.channel.send("WAJIB PAKAI PREFIX '!' SEBELUM PERINTAH DIBAWAH\nping = kalkulasi latency\nipk = show off ipk fauzi\nganteng = coba sendiri lah ya\nmukasaya = berkaca dulu gan");
+  }
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");       
+  }
   if (message.content.startsWith(config.prefix + "ipk")) {
     message.channel.send("ez 3.92 bosku");
   }
-  if (message.content.startsWith(config.prefix + "listperintah")) {
-    message.channel.send("WAJIB PAKAI PREFIX '!' SEBELUM PERINTAH DIBAWAH\nipk = show off ipk fauzi\nganteng = coba sendiri lah ya");
+  if (message.content.startsWith(config.prefix + "mukasaya")) {
+    message.reply(message.author.avatarURL);
   }
   if (message.content.startsWith(config.prefix + "ganteng")) {
     message.channel.send("Tampan dan Berani", {files: ["http://i65.tinypic.com/27y3ndx.jpg"]});
@@ -24,4 +30,4 @@ client.on('message', message => {
 });
 
 
-client.login(process.env.BOT_TOKEN);
+client.login(config.token);
