dca75b62cb92cbcb805b16496c1b0023d7edf69b
dematerial <36230364+dematerial@users.noreply.github.com>
2018-03-06 22:13:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('message', message => {
 
   if (message.content.startsWith(config.prefix + "level")) {
     sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-      if (!row) return message.reply("Your current level is 0");
+      if (!row) return message.reply("Level bacotmu masih 0, masih polos kamu");
       message.reply(`Level bacotmu ${row.level}`);
     });
   } else
